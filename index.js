9d37da191de2995b89d73aa610408801ccd3b753
Andrew Bernier <a6bernier@gmail.com>
2018-07-29 20:54:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  const newkittens = kittens.push(name)
+  const newKittens = kittens.push(name)
   return newKittens
 }
